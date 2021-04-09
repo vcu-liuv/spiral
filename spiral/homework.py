c5eb48e8ea3d71498afd4c681dff741ca9c80929
vcu-liuv <liuv@mymail.vcu.edu>
2021-04-09 19:45:05 -0400

    Homework

--- spiral/homework.py
+++ spiral/homework.py
@@ -1,4 +1,14 @@
 def spiralize(number):
-    return_value = 1
+    spiral_sum = 1
+    spiral_counter = 1
+    extra = number / 2
+    
+    curr_num = 1
+    
+    while spiral_counter < number - extra:
+        for x in range(4):
+            curr_num = curr_num + spiral_counter * 2
+            spiral_sum += curr_num
+        spiral_counter += 1
 
-    return return_value
+    return spiral_sum
